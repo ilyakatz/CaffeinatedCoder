89f99ef73fb3e6106a38bdef2b35b0fccc272038
Ilya Katz <ilyakatz@gmail.com>
2014-02-27 16:54:43 -0500

    first commit

---
+++ gen/com/perfectforloop/caffeinatedcoder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.perfectforloop.caffeinatedcoder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
